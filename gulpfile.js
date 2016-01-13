37d560bab89915364d9b0110ce2cf16313bc80b2
Tony <chiangloveslynn@gmail.com>
2016-01-14 00:10:55 +0800

    all the basics

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,11 @@ var gulp = require('gulp'),
         rename = require('gulp-rename'),
         jade = require('gulp-jade'),
         uglify = require('gulp-uglify'),
+        spritesmith = require('gulp.spritesmith'),
         concat = require('gulp-concat');
+
+const imagemin = require('gulp-imagemin');
+const pngquant = require('imagemin-pngquant');
 
 
 
@@ -36,6 +40,30 @@ gulp.task('styles', function() {
     .pipe(gulp.dest('dist/assets/stylesheets'));
 });
 
+//Make Sprites
+gulp.task('sprite', function() {
+    var spriteData = 
+        gulp.src('assets/images/icons/*.*') // source path of the sprite images
+            .pipe(spritesmith({
+                imgName: '../sprite.png',
+                cssName: 'sprite.scss',
+            }));
+
+    spriteData.img.pipe(gulp.dest('dist/assets/images/')); // output path for the sprite
+    spriteData.css.pipe(gulp.dest('assets/sass/lib/')); // output path for the CSS
+});
+
+//Imagemin
+gulp.task('image', () => {
+  return gulp.src('assets/images/**/*.*')
+    .pipe(imagemin({
+      progressive: true,
+      svgoPlugins: [{removeViewBox: false}],
+      use: [pngquant()]
+    }))
+    .pipe(gulp.dest('dist/assets/images'));
+});
+
 //Jade
 gulp.task('jade', function() {
   var YOUR_LOCALS = {};
@@ -48,7 +76,7 @@ gulp.task('jade', function() {
 
 // Scripts
 gulp.task('scripts', function() {
-  return gulp.src('assets/javascripts/**/*.js')
+  return gulp.src(['assets/javascripts/jquery.js','assets/javascripts/modernizr-custom.js','assets/javascripts/viewport.js','assets/javascripts/slidebars.min.js','assets/javascripts/jquery.sticky.js','assets/javascripts/jquery.bxslider.min.js','assets/javascripts/jquery.magnific-popup.min.js','assets/javascripts/all.js'])
     .pipe(concat('app.js'))
     .pipe(gulp.dest('dist/assets/javascripts'))
     .pipe(rename({ suffix: '.min' }))
@@ -60,11 +88,11 @@ gulp.task('scripts', function() {
 gulp.task('watch', function() {
   gulp.watch('assets/javascripts/**/*.js', ['scripts']);
   gulp.watch('assets/sass/**/*.scss', ['styles']);
-  gulp.watch('*.jade', ['jade']);
+  gulp.watch('**/*.jade', ['jade']);
   gulp.watch('dist/*.html', notifyLiveReload);
   gulp.watch('dist/assets/stylesheets/**/*.css', notifyLiveReload);
 });
 
-gulp.task('default', ['jade', 'scripts', 'styles', 'livereload', 'watch'], function() {
+gulp.task('default', ['jade', 'scripts','image', 'styles', 'sprite', 'livereload', 'watch'], function() {
 
 });
